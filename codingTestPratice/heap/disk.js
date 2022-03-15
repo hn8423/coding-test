f3485ac9846515354dce8ec6a1f31a324e17cdb5
김현인 <rolenche@gmail.com>
2022-03-15 09:47:04 +0900

    a

--- codingTestPratice/heap/disk.js
+++ codingTestPratice/heap/disk.js
@@ -6,7 +6,7 @@ const solution = (jobs) => {
     return a[0] - b[0];
   });
 
-  const priorityQueue = [1, 2]; //
+  const priorityQueue = [1, 2];
   while (j < jobs.length || priorityQueue.length !== 0) {
     if (jobs.length > j && time >= jobs[j][0]) {
       priorityQueue.push(jobs[j++]);
