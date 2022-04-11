dd0ac48b300a7f8e6d3ef16b36d9dde776a9e7e5
김현인 <rolenche@gmail.com>
2022-04-11 10:57:56 +0900

    a

--- codingTestPratice/level1/nanua.js
+++ codingTestPratice/level1/nanua.js
@@ -3,5 +3,15 @@
 
 function solution(arr, divisor) {
   var answer = [];
-  return answer;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      answer.push(arr[i]);
+    }
+  }
+
+  return answer.length === 0
+    ? [-1]
+    : answer.sort(function (a, b) {
+        return a - b;
+      });
 }
