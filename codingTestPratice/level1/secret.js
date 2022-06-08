bb825009e5fecc8cb2421c5522af9f38558b117f
김현인 <rolenche@gmail.com>
2022-06-08 13:20:11 +0900

    비밀 지도구현

--- codingTestPratice/level1/secret.js
+++ codingTestPratice/level1/secret.js
@@ -1,9 +1,13 @@
 function solution(n, arr1, arr2) {
   var answer = [];
-
-  return answer;
-
-  //이진수로 만들기
-  //각 자릿수 합치기: 공백 + 공백 = ' ', 1+1 = #, 1 + 공백 = #
-  //출력
+  let one = arr1.map((v) => {
+    return v.toString(2);
+  });
+  let two = arr2.map((v) => {
+    return v.toString(2);
+  });
+  let test = [39, 21].map((v) => {
+    return v.toString(2);
+  });
+  return test;
 }
