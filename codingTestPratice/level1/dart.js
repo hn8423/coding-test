6937cbb71c72b4c2e602c47b7a50e330e87fd8c5
hn8423 <rolenche@gmail.com>
2023-03-22 08:26:00 +0900

    다트게임

--- codingTestPratice/level1/dart.js
+++ codingTestPratice/level1/dart.js
@@ -1,23 +1,39 @@
-// 카카오톡 게임별의 하반기 신규 서비스로 다트 게임을 출시하기로 했다. 다트 게임은 다트판에 다트를 세 차례 던져 그 점수의 합계로 실력을 겨루는 게임으로, 모두가 간단히 즐길 수 있다.
-// 갓 입사한 무지는 코딩 실력을 인정받아 게임의 핵심 부분인 점수 계산 로직을 맡게 되었다. 다트 게임의 점수 계산 로직은 아래와 같다.
-
-// 다트 게임은 총 3번의 기회로 구성된다.
-// 각 기회마다 얻을 수 있는 점수는 0점에서 10점까지이다.
-// 점수와 함께 Single(S), Double(D), Triple(T) 영역이 존재하고 각 영역 당첨 시 점수에서 1제곱, 2제곱, 3제곱 (점수1 , 점수2 , 점수3 )으로 계산된다.
-// 옵션으로 스타상(*) , 아차상(#)이 존재하며 스타상(*) 당첨 시 해당 점수와 바로 전에 얻은 점수를 각 2배로 만든다. 아차상(#) 당첨 시 해당 점수는 마이너스된다.
-// 스타상(*)은 첫 번째 기회에서도 나올 수 있다. 이 경우 첫 번째 스타상(*)의 점수만 2배가 된다. (예제 4번 참고)
-// 스타상(*)의 효과는 다른 스타상(*)의 효과와 중첩될 수 있다. 이 경우 중첩된 스타상(*) 점수는 4배가 된다. (예제 4번 참고)
-// 스타상(*)의 효과는 아차상(#)의 효과와 중첩될 수 있다. 이 경우 중첩된 아차상(#)의 점수는 -2배가 된다. (예제 5번 참고)
-// Single(S), Double(D), Triple(T)은 점수마다 하나씩 존재한다.dsffs
-// 스타상(*), 아차상(#)은 점수마다 둘 중 하나만 존재할 수 있으며, 존재하지 않을 수도 있다.
-// 0~10의 정수와 문자 S, D, T, *, #로 구성된 문자열이 입력될 시 총점수를 반환하는 함수를 작성하라.
-
-for (let i = 0; i < dartResult.length; i++) {
-  // 점수가 10인 경우의 수
-  if (dartResult[i + 1] === "0") {
-    temp = dartResult[i] + dartResult[i + 1];
-    i++;
+function solution(dartResult) {
+  var score = 0;
+  var answer = [];
+  var temp = 0; // 숫자 담을 변수
+  for (let i = 0; i < dartResult.length; i++) {
+    if (dartResult[i] >= 0 && dartResult[i] <= 9) {
+      //숫자일 경우
+      if (dartResult[i] == 1 && dartResult[i + 1] == 0) {
+        // 10일 경우 10으로 만들어주고 0을 건너뛰기 위해 i++ 해줌.
+        temp = 10;
+        i++;
+      } else {
+        // 0이 아닐 경우 그대로 temp에 넣어줌.
+        temp = dartResult[i];
+      }
+    } else if (dartResult[i] === "S") {
+      // 1제곱
+      answer.push(temp);
+    } else if (dartResult[i] === "D") {
+      // 2제곱
+      answer.push(Math.pow(temp, 2));
+    } else if (dartResult[i] === "T") {
+      // 3제곱
+      answer.push(Math.pow(temp, 3));
+    } else if (dartResult[i] === "#") {
+      answer[answer.length - 1] *= -1; // 아차상, 해당 점수 -
+    } else if (dartResult[i] === "*") {
+      // 스타상, (직전 + 해당) *2
+      answer[answer.length - 1] *= 2;
+      answer[answer.length - 2] *= 2;
+    }
   }
+  for (let i = 0; i < answer.length; i++) {
+    // 보너스, 옵션 적용된 점수들 합치기
+    score += Number(answer[i]);
+  }
+
+  return score;
 }
-//
-//
