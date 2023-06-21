8649295888514552c3f429d7b77be45cb663a4b4
hn8423 <rolenche@gmail.com>
2023-06-21 12:58:42 +0900

    2023.0621

--- beakjun/index.js
+++ beakjun/index.js
@@ -4,39 +4,14 @@ let input = require("fs")
   .readFileSync(readFileSyncAddress)
   .toString()
   .split("\n");
+let N = Number(input[0]);
+let level = input.slice(1).map(Number).reverse();
+let count = 0;
 
-let [N, K] = input[0].split(" ").map(Number);
-let arr = input[1].split(" ").map(Number);
-
-let on = [];
-let answer = 0;
-
-for (let i = 0; i < K; i++) {
-  let cur = arr[i];
-  if (on.includes(cur)) continue;
-  if (on.length < N) {
-    on.push(cur);
-  } else {
-    let target;
-    let value = 0;
-
-    on.forEach((v) => {
-      let next = Infinity;
-      for (let j = i + 1; j < K; j++) {
-        if (arr[j] === v) {
-          next = j;
-          break;
-        }
-      }
-      if (value < next) {
-        target = v;
-        value = next;
-      }
-    });
-    on = on.filter((v) => v !== target);
-    on.push(cur);
-    answer++;
+for (let i = 0; i < N - 1; i++) {
+  if (level[i] <= level[i + 1]) {
+    count += level[i + 1] - level[i] + 1;
+    level[i + 1] = level[i] - 1;
   }
 }
-
-console.log(answer);
+console.log(count);
---
+++ beakjun/test.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,r = map(int, input().split())
+
+
+# 1.
+cache = [[0 for _ in range(r+1)] for _ in range(n+1)]
+
+# 2.
+for i in range(n+1):
+    cache[i][0] = 1
+for i in range(r+1):
+    cache[i][i] = 1
+
+# 3.
+for i in range(1, n+1):
+    for j in range(1, r+1):
+        cache[i][j] = cache[i-1][j] + cache[i-1][j-1]
+
+print( cache[n][r]%10007)
+
+
