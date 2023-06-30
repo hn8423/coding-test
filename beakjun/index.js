98ca24d0092a77dd9a43bfb6f26bba1cf490a062
hn8423 <rolenche@gmail.com>
2023-06-30 11:57:19 +0900

    2023.06.30

--- beakjun/index.js
+++ beakjun/index.js
@@ -1,18 +1,16 @@
-let m = 4;
-let section = [2, 3, 6];
-
-let cur = section[0];
-let count = 0;
-while (section.length) {
-  section.filter((v) => {
-    if (cur <= v && v < cur + m) {
-      return false;
-    } else {
-      return true;
+function solution(topping = [1, 2, 1, 3, 1, 4, 1, 2]) {
+  let answer = 0;
+  //for 구문을 돌려서 자르는 순서 확인 o(1)
+  //set을 사용하여 몇가지 토핑 맛보는지 확인 o(1)
+  //두 조각이 같은 갯수의 토핑이라면 카운트
+  for (let i = 0; i < topping.length; i++) {
+    let cake1 = new Set(topping.slice(0, i)).size;
+    let cake2 = new Set(topping.slice(i, -1)).size;
+    console.log(cake1 === cake2);
+    if (cake1 === cake2) {
+      answer++;
     }
-  });
-  count++;
-
-  cur += m;
+  }
+  console.log(answer);
 }
-console.log(count);
+solution();
