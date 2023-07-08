79e06fe677f4b39a22ca260a23c99d1df4b72776
hn8423 <rolenche@gmail.com>
2023-07-08 21:42:01 +0900

    2023.07.08

--- beakjun/index.js
+++ beakjun/index.js
@@ -1,18 +1,32 @@
-function solution(n, k) {
-  var answer = 1;
-  let temp = n;
-  while (true) {
-    if (temp % k === 0) {
-      break;
-    } else {
-      temp = Number(String(temp) + String(n));
+function solution(skill, skill_trees) {
+  let skillArr = skill.split("");
+  var answer = 0;
+  //스킬트리 for문 확인
+  for (let i = 0; i < skill_trees.length; i++) {
+    let lernSkill = [...skillArr];
+    let isPass = true;
+    //각 스킬트리 2중 for 문
+    for (let j = 0; j < skill_trees[i].length; j++) {
+      // 처음 스킬 선행 되는지 체크
+      if (lernSkill.indexOf(skill_trees[i][j]) !== -1) {
+        if (skill_trees[i][j] === lernSkill[0]) {
+          lernSkill.shift();
+        } else {
+          isPass = false;
+          break;
+        }
+      }
+    }
+    if (isPass) {
       answer++;
     }
-    // if (String(temp).length >= 10) {
-    //   return -1;
-    // }
   }
+  //안되면 break
+  //다 되면 카운트
+  // return answer;
+  // return skill_trees[0][0] in skillArr;
+  // return skill_trees[0][0];
   return answer;
 }
 
-console.log(solution(22, 9));
+console.log(solution("CBD", ["CBADF", "AECB", "BDA"]));
