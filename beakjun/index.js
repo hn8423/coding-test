127b8a41c2da2e7b4b3a1be2cdd5d686e57e332b
hn8423 <rolenche@gmail.com>
2023-07-09 09:10:16 +0900

    2023.07.09

--- beakjun/index.js
+++ beakjun/index.js
@@ -1,32 +1,22 @@
-function solution(skill, skill_trees) {
-  let skillArr = skill.split("");
-  var answer = 0;
-  //스킬트리 for문 확인
-  for (let i = 0; i < skill_trees.length; i++) {
-    let lernSkill = [...skillArr];
-    let isPass = true;
-    //각 스킬트리 2중 for 문
-    for (let j = 0; j < skill_trees[i].length; j++) {
-      // 처음 스킬 선행 되는지 체크
-      if (lernSkill.indexOf(skill_trees[i][j]) !== -1) {
-        if (skill_trees[i][j] === lernSkill[0]) {
-          lernSkill.shift();
-        } else {
-          isPass = false;
-          break;
-        }
-      }
-    }
-    if (isPass) {
-      answer++;
-    }
-  }
-  //안되면 break
-  //다 되면 카운트
-  // return answer;
-  // return skill_trees[0][0] in skillArr;
-  // return skill_trees[0][0];
-  return answer;
-}
+function solution(stones, k) {
+  // 이분탐색을 사용한다.
+  let left = 1;
+  let right = 200000000;
 
-console.log(solution("CBD", ["CBADF", "AECB", "BDA"]));
+  while (left <= right) {
+    const mid = ((left + right) / 2) >> 0;
+
+    let count = 0;
+    for (let i = 0; i < stones.length; i++) {
+      if (stones[i] - mid <= 0) count++;
+      else count = 0;
+
+      if (count === k) break;
+    }
+
+    if (count === k) right = mid - 1;
+    else left = mid + 1;
+  }
+
+  return left;
+}
