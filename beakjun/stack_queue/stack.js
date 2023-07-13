ee044fad04476b9d88141c2068b1d9f4987f9dca
hn8423 <rolenche@gmail.com>
2023-07-13 10:50:09 +0900

    2023.07.13

--- beakjun/stack_queue/stack.js
+++ beakjun/stack_queue/stack.js
@@ -13,3 +13,18 @@ function isValid(s) {
   }
   return !stack;
 }
+
+function dailyTemperatures(temperatures) {
+  let ans = new Array(temperatures.length).fill(0);
+  let stack = [];
+  for (let i = 0; i < temperatures.length; i++) {
+    while (stack && stack[-1][1] < temperatures[i]) {
+      let [prev_day, _] = stack.pop();
+      ans[prev_day] = i - prev_day;
+    }
+    stack.push([i, temperatures[i]]);
+  }
+  return ans;
+}
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
