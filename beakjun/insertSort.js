1918b60a2f97877ac2241d35a865b9dffe546fd0
hn8423 <rolenche@gmail.com>
2023-07-14 17:41:35 +0900

    2023.07.14

--- beakjun/insertSort.js
+++ beakjun/insertSort.js
@@ -1,6 +1,5 @@
 function insertionSort(arr) {
-  const len = arr.length;
-  for (let i = 1; i < len; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let current = arr[i];
     let j = i - 1;
     while (j >= 0 && arr[j] > current) {
@@ -13,5 +12,10 @@ function insertionSort(arr) {
 }
 
 const array = [5, 2, 4, 6, 1, 3];
+//254613
+// 4 245613
+// 6 245613
+// 1 124563
+// 3 123456
 const sortedArray = insertionSort(array);
 console.log(sortedArray); // [1,2,3,4,5,6]
