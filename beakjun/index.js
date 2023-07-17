9085f57eee82c14e8ff101c5fcde8940f0090d23
hn8423 <rolenche@gmail.com>
2023-07-17 13:35:39 +0900

    2023.07.17

--- beakjun/index.js
+++ beakjun/index.js
@@ -1,32 +1,41 @@
-function solution(wallpaper) {
-  let board = [];
-  for (let i = 0; i < wallpaper.length; i++) {
-    board.push(wallpaper[i].split(""));
-  }
+function solution(weights) {
+  var answer = 0;
+  //     한번씩 확인
+  //     하나 확인할때 j 랑 각각 비교 해야함
+  for (let i = 0; i < weights.length - 1; i++) {
+    for (let j = i + 1; j < weights.length; j++) {
+      let first = weights[i];
+      let next = weights[j];
 
-  let minx = Infinity;
-  let miny = Infinity;
-  let maxx = 0;
-  let maxy = 0;
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (board[i][j] === "#") {
-        minx = Math.min(minx, i);
-        miny = Math.min(miny, j);
-        maxx = Math.max(maxx, i);
-        maxy = Math.max(maxy, j);
+      if (first === next) {
+        //             22
+        answer++;
+      } else if (first * 2 === next * 3) {
+        //             23
+        answer++;
+      } else if (first * 3 === next * 2) {
+        //             32
+        answer++;
+      } else if (first * 2 === next * 4) {
+        //             24
+
+        answer++;
+      } else if (first * 4 === next * 2) {
+        //             42
+
+        answer++;
+      } else if (first * 4 === next * 3) {
+        //             43
+
+        answer++;
+      } else if (first * 3 === next * 4) {
+        //             34
+
+        answer++;
       }
     }
   }
-  return [minx, miny, maxx + 1, maxy + 1];
+  return answer;
 }
 
-console.log(
-  solution([
-    "..........",
-    ".....#....",
-    "......##..",
-    "...##.....",
-    "....#.....",
-  ])
-);
+console.log(solution([100, 180, 360, 100, 270]));
