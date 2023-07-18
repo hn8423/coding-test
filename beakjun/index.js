91de96fde03bde1983a319b19ea1d2949c9cb709
hn8423 <rolenche@gmail.com>
2023-07-18 14:17:45 +0900

    2023.07.18

--- beakjun/index.js
+++ beakjun/index.js
@@ -1,22 +1,15 @@
-function solution(weights) {
-  let answer = 0;
-  const store = {}; //key-value
-  const cal = [1, 3 / 2, 2, 4 / 3]; //경우의 수 (1,1), (2,3), (2,4), (3,4)
+function solution(s) {
+  var answer = 0;
+  let dp = new Array(s.length).fill([]);
 
-  weights
-    .sort((a, b) => b - a)
-    .forEach((w) => {
-      //내림차순 정렬 후, 전체 돌면서
-      let s;
-      cal.forEach((i) => {
-        if (((s = w * i), store[s])) {
-          //해당 비율을 곱한 값이 store에 존재할 경우
-          answer += store[s];
-        }
-      });
-      if (!store[w]) store[w] = 1;
-      else store[w]++;
-    });
+  for (let i = 0; i < s.length; i++) {}
+
   return answer;
 }
-console.log(solution([100, 180, 360, 100, 270]));
+console.log(solution("abcdcba"));
+// for (let j = i + 1; j <= s.length; j++) {
+//   let sub = s.substr(i, j);
+//   if (sub === sub.split("").reverse().join("")) {
+//     answer = Math.max(answer, sub.length);
+//   }
+// }
