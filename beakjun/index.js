31f52c02adf00b0b02c62f6c5a2f425ff3023297
hn8423 <rolenche@gmail.com>
2023-07-26 20:28:49 +0900

    2023.07.26

--- beakjun/index.js
+++ beakjun/index.js
@@ -1,9 +1,14 @@
-function solution(s) {
-  var answer = 0;
-  let dp = new Array(s.length).fill([]);
+function hanoiTower(n, from, to, aux) {
+  if (n === 1) {
+    console.log(`Move disk 1 from ${from} to ${to}`);
+    return;
+  }
 
-  for (let i = 0; i < s.length; i++) {}
-
-  return '';
+  hanoiTower(n - 1, from, aux, to);
+  console.log(`Move disk ${n} from ${from} to ${to}`);
+  hanoiTower(n - 1, aux, to, from);
 }
-console.log(solution("abcdcba"));
+
+// 예시
+const numDisks = 3; // 탑의 개수
+console.log(hanoiTower(numDisks, "1번 막대", "3번 막대", "2번 막대"));
