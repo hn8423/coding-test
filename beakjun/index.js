dd05d37cbc9f2af19d0e5d3a130b4f81819ef515
hn8423 <rolenche@gmail.com>
2023-07-27 14:01:35 +0900

    2023.07.27

--- beakjun/index.js
+++ beakjun/index.js
@@ -1,14 +1,32 @@
-function hanoiTower(n, from, to, aux) {
-  if (n === 1) {
-    console.log(`Move disk 1 from ${from} to ${to}`);
-    return;
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input = [];
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", async function () {
+  // 답안 작성
+  let answer = "";
+  let n = input[0];
+  let arr = "1001 " + input[1];
+  arr = arr.split(" ");
+  answer = max_decrease_array(n, arr);
+  console.log(answer);
+  process.exit();
+});
+
+function max_decrease_array(n, arr) {
+  let dp = new Array(parseInt(n) + 1).fill(0);
+  for (let i = 1; i <= n; i++) {
+    let max = 0;
+    for (let j = 0; j < i; j++) {
+      if (arr[j] * 1 > arr[i] * 1) {
+        max = Math.max(dp[j], max);
+      }
+    }
+    dp[i] = max + 1;
   }
-
-  hanoiTower(n - 1, from, aux, to);
-  console.log(`Move disk ${n} from ${from} to ${to}`);
-  hanoiTower(n - 1, aux, to, from);
+  return Math.max(...dp);
 }
-
-// 예시
-const numDisks = 3; // 탑의 개수
-console.log(hanoiTower(numDisks, "1번 막대", "3번 막대", "2번 막대"));
