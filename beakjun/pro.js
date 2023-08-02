ef8b4ad19779390c2bd991a310d667d9aeafccc4
hn8423 <rolenche@gmail.com>
2023-08-02 10:45:03 +0900

    2023.08.02

--- beakjun/pro.js
+++ beakjun/pro.js
@@ -1,78 +1,20 @@
-class MinHeap {
-  constructor() {
-    this.heap = [null];
-  }
-
-  size() {
-    return this.heap.length - 1;
-  }
-
-  getMin() {
-    return this.heap[1] ? this.heap[1] : null;
-  }
-
-  swap(a, b) {
-    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
-  }
-
-  heappush(value) {
-    this.heap.push(value);
-    let curIdx = this.heap.length - 1;
-    let parIdx = (curIdx / 2) >> 0;
-
-    while (curIdx > 1 && this.heap[parIdx] > this.heap[curIdx]) {
-      this.swap(parIdx, curIdx);
-      curIdx = parIdx;
-      parIdx = (curIdx / 2) >> 0;
+function solution(prices) {
+  var answer = new Array(prices.length).fill(0);
+  let stack = [];
+  for (let i = 0; i < prices.length; i++) {
+    while (stack.length !== 0 && stack[stack.length - 1][1] > prices[i]) {
+      let [prev_day, _] = stack.pop();
+      answer[prev_day] = i - prev_day;
     }
+    stack.push([i, prices[i]]);
   }
-
-  heappop() {
-    const min = this.heap[1];
-    if (this.heap.length <= 2) this.heap = [null];
-    else this.heap[1] = this.heap.pop();
-
-    let curIdx = 1;
-    let leftIdx = curIdx * 2;
-    let rightIdx = curIdx * 2 + 1;
-
-    if (!this.heap[leftIdx]) return min;
-    if (!this.heap[rightIdx]) {
-      if (this.heap[leftIdx] < this.heap[curIdx]) {
-        this.swap(leftIdx, curIdx);
-      }
-      return min;
-    }
-
-    while (
-      (!!this.heap[leftIdx] && this.heap[leftIdx] < this.heap[curIdx]) ||
-      (!!this.heap[rightIdx] && this.heap[rightIdx] < this.heap[curIdx])
-    ) {
-      const minIdx =
-        this.heap[leftIdx] > this.heap[rightIdx] ? rightIdx : leftIdx;
-      this.swap(minIdx, curIdx);
-      curIdx = minIdx;
-      leftIdx = curIdx * 2;
-      rightIdx = curIdx * 2 + 1;
-    }
-
-    return min;
+  if (stack.length) {
+    stack.forEach((v) => {
+      let [prev_day, _] = v;
+      answer[prev_day] = prices.length - 1 - prev_day;
+    });
   }
-}
-function solution(scoville, K) {
-  let heap = new MinHeap();
-  for (let i = 0; i < scoville.length; i++) {
-    heap.heappush(scoville[i]);
-  }
-  let answer = 0;
-  while (heap.getMin() < K && heap.size() > 0) {
-    answer++;
-    let first = heap.heappop();
-    let second = heap.heappop();
-    let sum = first + second * 2;
-    heap.heappush(sum);
-  }
-  return heap.getMin() >= K ? answer : -1;
+  return answer;
 }
 
-console.log(solution([1, 2, 3, 9, 10, 12], 7));
+console.log(solution([1, 2, 3, 2, 3]));
