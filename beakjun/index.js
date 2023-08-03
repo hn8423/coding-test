069877c7b229be749e8c9536c510c43b4307175f
hn8423 <rolenche@gmail.com>
2023-08-03 13:01:03 +0900

    2023.08.03

--- beakjun/index.js
+++ beakjun/index.js
@@ -6,53 +6,16 @@ let input = require("fs")
   .trim()
   .split("\n");
 
-const [M, N] = input[0].split(" ").map(Number);
-const dRow = [1, 0, -1, 0];
-const dCol = [0, 1, 0, -1];
-const board = [];
+const N = +input[0];
+const dp = Array(31).fill(0);
+dp[1] = 0;
+dp[2] = 3;
 
-for (let i = 1; i < M + 1; i++) {
-  board.push(input[i].split(" ").map(Number));
-}
+for (let i = 4; i <= N; i += 2) {
+  dp[i] = dp[i - 2] * 3 + 2;
 
-// 단순 DFS로 풀 때의 같은 노드 중복 재귀 호출을 피하기 위해, DP 메모이제이션 활용
-// 어떤 노드의 H는 상하좌우 값 작은 노드들의 H들의 합임. 부분 문제 정의
-// 중복 호출을 피하기 위해, 방문을 안 한 노드의 위치는 -1을 가지고,
-// 방문을 한 노드는 연산 결과, 0이나 그 이상의 H를 가짐
-// H가 0이라는 것은 그 노드 상하좌우에 내려갈 길이 없거나,
-// 내려갔는데 그 내려간 노드의 H가 모두 0인 경우이므로 즉
-// 마지막 노드까지 도달할 수 없는 경우라는 것이다.
-
-const DP = [];
-for (let i = 0; i < M; i++) {
-  DP.push([]);
-  for (let j = 0; j < N; j++) {
-    DP[i].push(-1);
+  for (let j = i - 4; j >= 0; j -= 2) {
+    dp[i] += dp[j] * 2;
   }
 }
-
-function DFS(row, col) {
-  if ((row === M - 1) & (col === N - 1)) {
-    return 1;
-  }
-
-  if (DP[row][col] != -1) {
-    return DP[row][col];
-  }
-
-  let H = 0;
-  for (let i = 0; i < 4; i++) {
-    let nRow = row + dRow[i];
-    let nCol = col + dCol[i];
-    if ((nRow >= 0) & (nRow < M) & (nCol >= 0 % nCol < N)) {
-      if (board[nRow][nCol] < board[row][col]) {
-        H += DFS(nRow, nCol);
-      }
-    }
-  }
-
-  DP[row][col] = H;
-  return H;
-}
-
-console.log(DFS(0, 0));
+console.log(dp[N]);
--- beakjun/sample.js
+++ beakjun/sample.js
@@ -1,43 +1,30 @@
-//f = all floor count
-// s = start
-// g = interview floor
-// u = UP
-// d = DOWN
-//fail = use the stair
-//bfs_1차원_count
-const readFileSyncAddress =
-  process.platform === "linux" ? "/dev/stdin" : "example.txt";
-let input = require("fs").readFileSync(readFileSyncAddress).toString().trim();
-
-let [f, s, g, u, d] = input.split(" ").map(Number);
-
-let visited = new Array(1000001).fill(0);
-let button = [u, d];
-let answer = 0;
-
-// //BFS
-
-function BFS(v) {
-  let queue = [[v, 0]];
-  while (queue.length) {
-    let [x, cnt] = queue.shift();
-    if (visited[x] === 1) {
-      continue;
-    }
-    if (x === g) {
-      console.log(cnt);
-      break;
-    }
-    //올라갈지 말지 결정
-    for (let stairs of [x + u, x - d]) {
-      stairs >= 1 &&
-        stairs <= f &&
-        !visited[x] &&
-        queue.push([stairs, cnt + 1]);
-    }
-    visited[x] = 1;
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
   }
-  console.log("use the stairs");
+
+  // 피벗을 선택합니다. 일반적으로 배열의 가운데 요소를 선택하거나 랜덤하게 선택할 수도 있습니다.
+  const pivot = arr[Math.floor(arr.length / 2)];
+
+  // 피벗보다 작은 요소들과 큰 요소들을 담을 빈 배열을 생성합니다.
+  const left = [];
+  const right = [];
+
+  // 피벗을 기준으로 배열을 분할합니다.
+  for (const element of arr) {
+    if (element < pivot) {
+      left.bpush(element);
+    } else if (element > pivot) {
+      right.push(element);
+    }
+    // 피벗과 같은 경우에는 무시합니다.
+  }
+
+  // 분할된 배열들을 재귀적으로 정렬하고, 피벗과 함께 합쳐서 정렬된 배열을 반환합니다.
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
-BFS(s);
+// 예시
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = quickSort(unsortedArray);
+console.log(sortedArray); // [11, 12, 22, 25, 34, 64, 90]
