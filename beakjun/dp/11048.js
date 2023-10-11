1c13d39f36490b6cd875c35dfce0b769d3eb8e38
hn8423 <rolenche@gmail.com>
2023-10-11 10:24:31 +0900

    11048

---
+++ beakjun/dp/11048.js
@@ -0,0 +1,26 @@
+const readFileSyncAddress =
+  process.platform === "linux" ? "/dev/stdin" : "../example.txt";
+let input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trim()
+  .split("\n");
+
+let [N, M] = input[0].split(" ").map(Number);
+const arr = Array(N)
+  .fill()
+  .map((_, i) => input[i + 1].split(" ").map(Number));
+const dp = Array(N)
+  .fill()
+  .map((_) => Array(M).fill(0));
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    const top = i > 0 ? dp[i - 1][j] : 0;
+    const left = j > 0 ? dp[i][j - 1] : 0;
+    const corner = i > 0 && j > 0 ? dp[i - 1][j - 1] : 0;
+    dp[i][j] = arr[i][j] + Math.max(top, left, corner);
+  }
+}
+
+console.log(dp[N - 1][M - 1]);
