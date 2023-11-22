d1f80c552b7eabc304743e49299b116a3d1ea7ea
hn8423 <rolenche@gmail.com>
2023-11-23 08:50:44 +0900

    2023.11.23

--- codingTestPratice/level0/181898.py
+++ codingTestPratice/level0/181898.py
@@ -1,8 +1,13 @@
 def solution(arr, idx):
-    answer = -1
-    for i in range(arr):
-        if i > idx :
-            if arr[i] == 1 :
-                answer = i
-                return
+    answer = 0
+    if(idx == 0 and arr[0] ==1):
+        return 0
+    
+    for i in range(idx, len(arr)):
+        
+        if arr[i] == 1:
+            answer = i
+            break
+    if answer == 0:
+        answer = -1
     return answer
