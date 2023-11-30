f8bc57b18631d0a5fcb15cfba99db77b8ad64021
hn8423 <rolenche@gmail.com>
2023-12-01 08:45:46 +0900

    2023.12.01

---
+++ beakjun/dp/14002.js
@@ -0,0 +1,31 @@
+const readFileSyncAddress =
+  process.platform === "linux" ? "/dev/stdin" : "../example.txt";
+let input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trim()
+  .split("\n");
+
+let N = Number(input[0]);
+let A = input[1].split(" ").map(Number);
+
+let dp = Array.from({ length: N }, () => 0);
+let arr = [];
+
+for (let i = 0; i < N; i++) {
+  let max = 0;
+  let maxIdx = -1;
+  for (let j = 0; j < i; j++) {
+    if (A[i] > A[j] && dp[j] > max) {
+      max = dp[j];
+      maxIdx = j;
+    }
+  }
+  dp[i] = max + 1;
+  arr[i] = maxIdx !== -1 ? arr[maxIdx].concat(A[i]) : [A[i]];
+}
+
+let answer = Math.max(...dp);
+
+console.log(answer);
+console.log(arr[dp.indexOf(answer)].join(" "));
