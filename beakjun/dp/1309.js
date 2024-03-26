24ea668d67ae6ecbab38d18d06a723d67dd40479
hn8423 <rolenche@gmail.com>
2024-03-26 15:02:55 +0900

    master 2

--- beakjun/dp/1309.js
+++ beakjun/dp/1309.js
@@ -4,6 +4,7 @@ let input = require("fs").readFileSync(readFileSyncAddress).toString();
 // ㅣㅗㄹㅑㅕㄴㅇ모ㅑㅕ
 input = Number(input);
 //master 1
+//master 2
 let dp = [1, 3];
 for (let i = 2; i <= input; i++) {
   dp[i] = (dp[i - 1] * 2 + dp[i - 2]) % 9901;
