e9890a458e28c36023632f65a09bd7a0fd688bac
hn8423 <rolenche@gmail.com>
2024-04-09 10:09:01 +0900

    fsadgas

--- beakjun/binary_search/sample.js
+++ beakjun/binary_search/sample.js
@@ -2,10 +2,11 @@ function solution(stones, k) {
   // 이분탐색을 사용한다.
   let left = 1;
   let right = 200000000;
+  // 문제에서 주어진 조건에 따라 최대값을 설정한다.
 
   while (left <= right) {
     const mid = ((left + right) / 2) >> 0;
-// 
+
     let count = 0;
     for (let i = 0; i < stones.length; i++) {
       if (stones[i] - mid <= 0) count++;
