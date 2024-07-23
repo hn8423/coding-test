73f5587d1649780cdf638294edbc1c6763b7adaf
hn8423 <rolenche@gmail.com>
2024-07-23 12:51:59 +0900

    2024.07.23 코테

--- temp.js
+++ temp.js
@@ -1,15 +1,34 @@
-function solution(my_string, indices) {
-    var answer = '';
-    for(let i =0; i < my_string.length; i++){
-        let char = my_string[i]
-        if(indices.includes(i)){
-            continue
-        }else{
-        answer += char   
+function solution(board, moves) {
+
+    function pickup(number){
+        for(let i = 0; i < board.length; i++){
+            if(board[i][number-1] !== 0){
+                let pick = board[i][number-1]
+                board[i][number-1] = 0
+                return pick
+            }
         }
+    }
+    var answer = 0;
+    let stack = []
+    for(let i =0; i < moves.length; i++){
+        let cur = pickup(moves[i])
+        
+        if(!cur){
+            continue
+        }
+        if(cur === stack[stack.length -1] ){
+            stack.pop()                
+                answer += 2
+            }else{
+                stack.push(cur)
+            }
         
     }
     return answer;
+
 }
 
-solution("apporoograpemmemprs",	[1, 16, 6, 15, 0, 10, 11, 3])
+
+
+console.log(solution( [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],	[1,5,3,5,1,2,1,4]))
